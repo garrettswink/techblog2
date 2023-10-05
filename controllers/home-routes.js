ba310fc355cde9c8e6232a20778aa165a9d26118
Garrett <garrettswink421@gmail.com>
2023-10-05 14:11:04 -0400

    Final Updates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,10 +2,6 @@ const router = require('express').Router();
 const { Post, User, Comment } = require("../models");
 const withAuth = require('../utils/auth');
 
-
-
-
-
 // Render Homepage with All Posts
 router.get('/', async (req, res) => {
     try {
